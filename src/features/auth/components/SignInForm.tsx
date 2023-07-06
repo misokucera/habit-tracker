f4f866fc2abf987a028d20d39cf2c40ad2bfb352
Michal Kučera <miso.kucera97@gmail.com>
2023-07-06 14:55:09 +0200

    Add anonymous sign in option

--- src/features/auth/components/SignInForm.tsx
+++ src/features/auth/components/SignInForm.tsx
@@ -4,6 +4,7 @@ import { auth } from "@/services/firebase";
 import {
     AuthProvider,
     GoogleAuthProvider,
+    signInAnonymously,
     signInWithPopup,
 } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
@@ -16,13 +17,25 @@ const SignInForm = () => {
     };
 
     return (
-        <div>
+        <div className="">
             <button
                 className="inline-flex items-center gap-3 px-4 py-2 rounded-md border text-sm whitespace-nowrap"
                 onClick={() => singIn(provider)}
             >
                 <FcGoogle />
                 Sign in with Google
+            </button>
+            <div className="relative mt-10 mb-5">
+                <hr />
+                <span className="relative -top-3 bg-white text-xs text-slate-500 px-4">
+                    OR
+                </span>
+            </div>
+            <button
+                className="inline-flex items-center gap-3 px-4 py-2 rounded-md border text-sm whitespace-nowrap"
+                onClick={() => signInAnonymously(auth)}
+            >
+                Sign in Anonymously
             </button>
         </div>
     );
