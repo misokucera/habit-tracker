4cf2953dca9a496a2ed14a44fce1677be153d0b9
Michal Kučera <miso.kucera97@gmail.com>
2023-07-24 16:08:55 +0200

    Add cva library

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,11 +1,24 @@
-import classNames from "classnames";
+import { VariantProps, cva } from "class-variance-authority";
+
+const variants = cva(
+    "rounded px-4 py-2 text-sm font-semibold transition-colors focus:outline-none focus-visible:ring-4",
+    {
+        variants: {
+            variant: {
+                primary:
+                    "bg-sky-400 text-white hover:bg-sky-500 focus-visible:ring-sky-200",
+                secondary:
+                    "bg-slate-200 text-slate-500 hover:bg-slate-300 hover:text-slate-600 focus-visible:ring-slate-100",
+            },
+        },
+    },
+);
 
 type Props = {
     children: React.ReactNode;
     type?: "submit" | "button";
-    variant?: "primary" | "secondary";
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
-};
+} & VariantProps<typeof variants>;
 
 const Button = ({
     onClick,
@@ -14,19 +27,7 @@ const Button = ({
     variant = "primary",
 }: Props) => {
     return (
-        <button
-            className={classNames(
-                "rounded px-4 py-2 text-sm font-semibold transition-colors focus:outline-none focus-visible:ring-4",
-                {
-                    "bg-sky-400 text-white hover:bg-sky-500 focus-visible:ring-sky-200":
-                        variant === "primary",
-                    "bg-slate-200 text-slate-500 hover:bg-slate-300 hover:text-slate-600 focus-visible:ring-slate-100":
-                        variant === "secondary",
-                },
-            )}
-            onClick={onClick}
-            type={type}
-        >
+        <button className={variants({ variant })} onClick={onClick} type={type}>
             {children}
         </button>
     );
--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -1,12 +1,23 @@
-import classNames from "classnames";
 import React from "react";
 import { forwardRef } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+
+const variants = cva(
+    "inline-flex h-10 w-10 items-center justify-center rounded-full  transition-colors hover:bg-slate-100",
+    {
+        variants: {
+            variant: {
+                light: "text-slate-300 hover:text-slate-500",
+                normal: "text-slate-500",
+            },
+        },
+    },
+);
 
 type Props = {
     children: React.ReactNode;
-    variant?: "light" | "normal";
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
-};
+} & VariantProps<typeof variants>;
 
 const IconButton = forwardRef<HTMLButtonElement, Props>(
     ({ children, onClick, variant = "normal", ...props }: Props, ref) => {
@@ -15,14 +26,7 @@ const IconButton = forwardRef<HTMLButtonElement, Props>(
                 {...props}
                 onClick={onClick}
                 ref={ref}
-                className={classNames(
-                    "inline-flex h-10 w-10 items-center justify-center rounded-full  transition-colors hover:bg-slate-100",
-                    {
-                        "text-slate-300 hover:text-slate-500":
-                            variant === "light",
-                        "text-slate-500": variant === "normal",
-                    },
-                )}
+                className={variants({ variant })}
             >
                 {children}
             </button>
