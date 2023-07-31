d53ac40f299246919f2652913b355480e9a23ce4
Michal Kučera <miso.kucera97@gmail.com>
2023-07-31 11:51:58 +0200

    Improve buttons with tailwind-merge

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -1,6 +1,7 @@
 import React, { ComponentProps } from "react";
 import { forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
 
 const variants = cva(
     "inline-flex h-10 w-10 items-center justify-center rounded-full  transition-colors hover:bg-slate-100",
@@ -14,14 +15,16 @@ const variants = cva(
     },
 );
 
-type ButtonPropsWithoutClassName = Omit<ComponentProps<"button">, "className">;
-
-type Props = ButtonPropsWithoutClassName & VariantProps<typeof variants>;
+type Props = ComponentProps<"button"> & VariantProps<typeof variants>;
 
 const IconButton = forwardRef<HTMLButtonElement, Props>(
-    ({ children, variant = "normal", ...props }: Props, ref) => {
+    ({ children, variant = "normal", className, ...props }: Props, ref) => {
         return (
-            <button {...props} ref={ref} className={variants({ variant })}>
+            <button
+                {...props}
+                ref={ref}
+                className={twMerge(variants({ variant }), className)}
+            >
                 {children}
             </button>
         );
