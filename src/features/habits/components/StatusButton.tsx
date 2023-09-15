2135da4d4fdd84b5172106f05d96b4e2c9ad5945
Michal Kučera <miso.kucera97@gmail.com>
2023-09-15 14:02:46 +0200

    Change unsuccessful color to red

--- src/features/habits/components/StatusButton.tsx
+++ src/features/habits/components/StatusButton.tsx
@@ -33,7 +33,7 @@ const StatusButton = ({
         }
 
         if (dayInPast > 0) {
-            return "bg-amber-100";
+            return "bg-red-100";
         }
 
         if (isOptional) {
@@ -58,7 +58,7 @@ const StatusButton = ({
                     "rounded-full p-3 transition-colors focus:outline-none",
                     {
                         "hover:bg-lime-200 focus-visible:bg-lime-200": status,
-                        "hover:bg-amber-200 focus-visible:bg-amber-200":
+                        "hover:bg-red-200 focus-visible:bg-red-200":
                             !status && dayInPast > 0,
                         "hover:bg-slate-100 focus-visible:bg-slate-100":
                             !status && dayInPast === 0,
@@ -68,7 +68,7 @@ const StatusButton = ({
                 <div
                     className={classNames("h-3 w-3 rounded-full", {
                         "bg-lime-500": status,
-                        "bg-amber-500": !status && dayInPast > 0,
+                        "bg-red-400": !status && dayInPast > 0,
                         "bg-slate-300": !status && dayInPast === 0,
                     })}
                 ></div>
