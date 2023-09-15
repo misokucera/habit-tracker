e3d77e3b7a2c121775ad17414b3bf7bfefc34a7b
Michal Kučera <miso.kucera97@gmail.com>
2023-09-15 12:30:52 +0200

    Fix prettier

--- src/features/habits/components/ChangeStatusButton.tsx
+++ src/features/habits/components/ChangeStatusButton.tsx
@@ -24,27 +24,27 @@ const ChangeStatusButton = ({
 
     return (
         <div>
-        <button
-            onClick={handleClick}
-            className={classNames(
-                "rounded-full p-3 transition-colors focus:outline-none",
-                {
-                    "hover:bg-lime-200 focus-visible:bg-lime-200": status,
-                    "hover:bg-amber-200 focus-visible:bg-amber-200":
-                        !status && dayInPast > 0,
-                    "hover:bg-slate-100 focus-visible:bg-slate-100":
-                        !status && dayInPast === 0,
-                },
-            )}
-        >
-            <div
-                className={classNames("h-3 w-3 rounded-full", {
-                    "bg-lime-500": status,
-                    "bg-amber-500": !status && dayInPast > 0,
-                    "bg-slate-300": !status && dayInPast === 0,
-                })}
-            ></div>
-        </button>
+            <button
+                onClick={handleClick}
+                className={classNames(
+                    "rounded-full p-3 transition-colors focus:outline-none",
+                    {
+                        "hover:bg-lime-200 focus-visible:bg-lime-200": status,
+                        "hover:bg-amber-200 focus-visible:bg-amber-200":
+                            !status && dayInPast > 0,
+                        "hover:bg-slate-100 focus-visible:bg-slate-100":
+                            !status && dayInPast === 0,
+                    },
+                )}
+            >
+                <div
+                    className={classNames("h-3 w-3 rounded-full", {
+                        "bg-lime-500": status,
+                        "bg-amber-500": !status && dayInPast > 0,
+                        "bg-slate-300": !status && dayInPast === 0,
+                    })}
+                ></div>
+            </button>
         </div>
     );
 };
